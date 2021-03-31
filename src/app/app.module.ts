a49fee2f8d3dcb1ee224f1e485149686645a7c27
Bishal Giri <giribsaal@gmail.com>
2021-03-31 22:48:29 +0545

    [Enhancement-Setup] Initial project configuration with header and footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
