d8cbfb43aea1106fc39e105a29e279783ce7d280
Bishal Giri <giribsaal@gmail.com>
2021-04-05 21:38:37 +0545

    [Feature-Component] Following components added 1. Science 2. Technology 3. Gadgets 4. Laptop 5. Gaming 6. Blogs 7. Contact Us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,34 @@ import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { GadgetComponent } from './gadget/gadget.component';
+import { VideoComponent } from './video/video.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ScienceComponent } from './science/science.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { GameComponent } from './game/game.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent,
+    HomeComponent,
+    DetailComponent,
+    GadgetComponent,
+    VideoComponent,
+    ReviewsComponent,
+    ScienceComponent,
+    TechnologyComponent,
+    LaptopComponent,
+    GameComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -1,7 +1,65 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/** Components import */
+import { HomeComponent } from '../home/home.component';
+import { DetailComponent } from '../detail/detail.component';
+import { ContactComponent } from '../contact/contact.component';
+import { GadgetComponent } from '../gadget/gadget.component';
+import { VideoComponent } from '../video/video.component';
+import { ReviewsComponent } from '../reviews/reviews.component';
+import { ScienceComponent } from '../science/science.component';
+import { TechnologyComponent } from '../technology/technology.component';
+import { LaptopComponent } from '../laptop/laptop.component';
+import { GameComponent } from '../game/game.component';
+import { BlogComponent } from '../blog/blog.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'tech-detail/:id',
+    component: DetailComponent
+  },
+  {
+    path: 'science',
+    component: ScienceComponent
+  },
+  {
+    path: 'technology',
+    component: TechnologyComponent
+  },
+  {
+    path: 'gadgets',
+    component: GadgetComponent
+  },
+  {
+    path: 'laptop',
+    component: LaptopComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  },
+  {
+    path: 'video',
+    component: VideoComponent
+  },
+  {
+    path: 'reviews',
+    component: ReviewsComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
